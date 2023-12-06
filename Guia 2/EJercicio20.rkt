04192a9022b786ecf9816177ecba452d5798ccf4
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2023-12-06 14:50:16 -0300

    Ejercicio19

---
+++ Guia 2/EJercicio20.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+;dado una lista y un elemento, obtener una lista con todos los elementos mayores a el
+; y otra lista con todos los elementos menos a el
+
+(define (separarListas lista elemento)
+    (define (mayores lista elemento)
+        (cond 
+            ((null? lista) '())
+            ((> (car lista) elemento) (cons (car lista) (mayores (cdr lista) elemento)))
+            (else (mayores (cdr lista) elemento))
+        )
+    )
+    (define (menores lista elemento)
+        (cond 
+            ((null? lista) '())
+            ((<= (car lista) elemento) (cons (car lista) (menores (cdr lista) elemento)))
+            (else (menores (cdr lista) elemento))
+        )
+    )
+    (display (menores lista elemento))
+    (newline)
+    (display (mayores lista elemento))
+    
+)
