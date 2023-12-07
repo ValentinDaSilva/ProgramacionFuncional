5ae5b490efee6e1ca1553c5c5ec637b33758bbb2
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2023-12-07 01:30:29 -0300

    agregado de ejercicios

---
+++ Guia 2/Ejercicio24.rkt
@@ -0,0 +1,28 @@
+#lang racket
+
+(define (reversa lista)
+ (if (null? (cdr lista))
+     lista
+     (cons (ultimoElemento lista) (cons (reversa (eliminarUltimoElemento lista)) '()))
+  )
+)
+
+(define (ultimoElemento lista)
+     (if (null? (cdr lista))
+         (car lista)
+         (ultimoElemento (cadr lista))
+     )
+)
+
+(define lista '(3(1(2)))
+
+(define (eliminarUltimoElemento lista)
+(if (null? (cdr lista))
+    '()
+  (if (null? (cdr (cadr lista)))
+         (cons (car lista) '())
+         (cons (car lista) (cons (eliminarUltimoElemento (cadr lista)) '()))
+     )
+  )
+)
+
