038c7aa7b37937a6bb4a63225936a88ad618c7cb
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2023-12-08 15:19:43 -0300

    FInEj30

--- Guia 2/Ejercicio30.rkt
+++ Guia 2/Ejercicio30.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 ;PUTPROP es una lista BasedeDatos, si al incorporar una (objeto atributo) ya existe debe sobreponerse y si no crearse
-
+;GETPROP devuelve el valor asociado al atribuo y objeto
 (define (PUTPROP BaseDeDatos objeto atributo valor) 
     (if (null? BaseDeDatos)
         (cons (list objeto atributo valor) '())
@@ -9,5 +9,12 @@
             (cons (list objeto atributo valor) (cdr BaseDeDatos))
             (cons (car BaseDeDatos) (PUTPROP (cdr BaseDeDatos) objeto atributo valor))
         )
+    )
+)
+
+(define (GETPROP BaseDeDatos objeto atributo)
+    (if (and (equal?(caar BaseDeDatos) objeto) (equal?(cadr (car BaseDeDatos)) atributo))
+        (caddr(car BaseDeDatos))
+        (GETPROP (cdr BaseDeDatos) objeto atributo)
     )
 )
